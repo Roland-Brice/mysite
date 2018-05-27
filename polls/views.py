54348142e6a7ef0cbab5f2dd54ebcb2d43faa70c
Roland-Brice <okalabrice@gmail.com>
2018-05-27 18:37:28 +0000

    Random fixes

--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello World Your in the index")
+    return HttpResponse("Hello, world. You're at the polls index.")
